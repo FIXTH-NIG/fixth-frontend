04c4a2dc9f6356dc4220f90dc92007d7eb9a7fde
osamos <ogundanajoseph17@gmail.com>
2026-03-02 21:03:40 +0100

    swap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import LandingPage from "./pages/LandingPage"
 import AuthPage from "./pages/AuthPage"
+import MainPage from "./pages/MainPage"
 
 function App() {
 
   return (
     <div className="App">
-      <AuthPage/>
+      <MainPage/>
     </div>
   )
 }
